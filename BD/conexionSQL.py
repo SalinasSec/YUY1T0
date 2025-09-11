4373493cb0de1deb894b5316aeebaeba9f06476c
SalinasSec <r.salinascasas@liceorbl.cl>
2025-09-10 21:03:04 -0300

    privar bd

--- BD/conexionSQL.py
+++ BD/conexionSQL.py
@@ -9,9 +9,9 @@ class DAO:
         try:
             return connect(
                 host="localhost",
-                user="root",
-                password="abcd1234",
-                database="clinica",
+                user="USUARIO",
+                password="CONTRASEÑA",
+                database="BD",
             )
         except Error as ex:
             print("Error conectar base de datos", ex)
